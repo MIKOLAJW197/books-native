5f3b39946afa88466f45e98ef600fb259ca3f142
mwolicki <mikolajw1997@gmail.com>
2019-11-17 17:28:11 +0100

    detailsPage update

--- app/screens/BookDetails.js
+++ app/screens/BookDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { ActivityIndicator } from 'react-native'
-import { Text, View } from "native-base";
+import { ActivityIndicator, ScrollView } from 'react-native'
+import { Text, View, Thumbnail } from "native-base";
 import axios from "axios";
 
 
@@ -14,26 +14,52 @@ const BookDetails = ({ navigation }) => {
         axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
             .then(
                 response => {
-                    setDetails(response.data);
+                    setDetails(response.data.volumeInfo);
                 },
                 error => console.log(error)
             )
     }, []);
 
+    const authorsList = details ? details.authors.map((item) =>
+        <Text key={item}>{item}</Text>
+    ) : 'No authors info!';
+
     if (details) {
         return (
-            <View style={{ flex: 1 }}>
-                <Text>
-                    {details.volumeInfo.title}
-                </Text>
-            </View>
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <View style={{ flex: 1, alignItems: 'center' }}>
+                        <Thumbnail
+                            style={{ flex: 1 }}
+                            square
+                            source={details.imageLinks ?
+                                { uri: details.imageLinks.small }
+                                : require('../../assets/icon.png')}>
+                        </Thumbnail>
+                    </View>
+                    <View style={{ flex: 2, flexDirection: 'column' }}>
+                        <Text>
+                            {details.title}
+                        </Text>
+                        <Text>
+                            {details.subtitle}
+                        </Text>
+                        {authorsList}
+                    </View>
+                </View>
+                <View style={{ flex: 8 }}>
+                    <Text>
+                        {details.description}
+                    </Text>
+                </View>
+            </ScrollView>
         )
     }
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <ActivityIndicator size="large" color="#0000ff"></ActivityIndicator>
         </View>
-        )
-    };
-    
+    )
+};
+
 export default BookDetails;
